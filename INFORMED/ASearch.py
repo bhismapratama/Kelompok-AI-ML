649e5748cdc01c8a524f672b6fef4b3f940175ee
bhismapratama <bhismaelki@gmail.com>
2024-04-02 14:25:58 +0700

    fix: fixixixi

--- INFORMED/ASearch.py
+++ INFORMED/ASearch.py
@@ -1,9 +1,13 @@
 import time
 
+# in case merupakan jumlah stasiun yang ada
 V = 6
 
 def min_cost(cost, visited):
-    min_val = float('inf')
+
+    # initialisasi biaya minimum
+    min_val = float('inf') 
+    # initialisasi index minimum
     min_index = -1
     for v in range(V):
         if not visited[v] and cost[v] < min_val:
@@ -14,19 +18,25 @@ def min_cost(cost, visited):
 def print_path(parent, j):
     if parent[j] == -1:
         return
+    # rekursif mencetak jalur
     print_path(parent, parent[j])
     print(" ->", j, end='')
 
 def print_solution(cost, parent, src, dest):
+    # fungsi untuk mencetak jalur yang ditemukan dari stasiun awal ke stasiun tujuan, beserta total biaya perjalanan.
     print("Path from", src, "to", dest, ":", src, end='')
     print_path(parent, dest)
     print("\nTotal Cost:", cost[dest])
 
 def a_star_search(graph, src, dest, heuristic):
+    # array untuk menyimpan orang tua dari setiap stasiun yang dikunjungi.
     parent = [-1] * V
+    # array untuk menandai stasiun yang telah dikunjungi.
     visited = [False] * V
+    # array untuk menyimpan biaya terpendek dari stasiun awal ke setiap stasiun.
     cost = [float('inf')] * V
 
+    # karan source awal adalah stasiun awal, jadi tidak ada biaya
     cost[src] = 0
 
     for _ in range(V - 1):
